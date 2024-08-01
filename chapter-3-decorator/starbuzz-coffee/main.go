bd06b03bf5108534af0540b04364bccf986bfcc5
hanke <hankeyyh@outlook.com>
2024-08-01 20:43:53 +0800

    feat: useless comment

--- chapter-3-decorator/starbuzz-coffee/main.go
+++ chapter-3-decorator/starbuzz-coffee/main.go
@@ -4,19 +4,12 @@ import (
 	"fmt"
 )
 
-/*
-Beverage 必须是interface, 不然condiment没办法嵌套各种beverage
-condiment 需要调用 beverage.getSize 决定了interface中要包含getSize方法
-beverage interface 不需要包含setSize, 不然condiment也可以调setsize有点奇怪,
-setSize可以放在各种concrete beverage里
-*/
 func main() {
 	espresso := NewEspresso()
 	fmt.Printf("%s, $%.2f\n", espresso.GetDescription(), espresso.Cost())
 
 	darkRoast := NewDarkRoast()
 	fmt.Printf("%s, $%.2f\n", darkRoast.GetDescription(), darkRoast.Cost())
-	// TODO value receiver not suitable for embedded pointer receiver
 	finalDarkRoast := NewWhip(NewMocha(NewMocha(&darkRoast)))
 	fmt.Printf("%s, $%.2f\n", finalDarkRoast.GetDescription(), finalDarkRoast.Cost())
 
